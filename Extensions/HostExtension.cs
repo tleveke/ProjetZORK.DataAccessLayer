77e9fcc04275bebdb34ac95902fda992e5f8a75f
BIGEARD Robin <robin.bigeard@ynov.com>
2021-02-12 16:05:26 +0100

    feat(): add seed database and run async

---
+++ Extensions/HostExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace ProjetZORK
+{
+    public static class HostExtension
+    {
+        public static IHost SeedDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                SeedApplicationData(scope);
+            }
+            return host;
+        }
+
+        private static void SeedApplicationData(IServiceScope scope)
+        {
+            var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
+            try
+            {
+                Task.WaitAll(seeder.EnsureSeedDataAsync());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Seed application data Error : [O]", ex.Message));
+            }
+        }
+
+    }
+}
---
+++ Seeders/Seeder.cs
@@ -0,0 +1,31 @@
+﻿using ProjetZORK.DataAccessLayer;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetZORK
+{
+    public class Seeder
+    {
+        private readonly ZorkManagerDbContext dbContext;
+
+        public Seeder(ZorkManagerDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task EnsureSeedDataAsync()
+        {
+/*            if (!this.dbContext.Weapons.Any())
+            {
+                await this.dbContext.AddRangeAsync(WeaponsSeeder.Seed()).ConfigureAwait(false);
+            }
+
+            if (!this.dbContext.Spots.Any())
+            {
+                await this.dbContext.AddRangeAsync(SpotsSeeder.Seed()).ConfigureAwait(false);
+            }*/
+            await this.dbContext.SaveChangesAsync();
+        }
+
+    }
+}
